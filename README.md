d12c67decccf67859a776fa716f7e07da8c7fa68
rsancho64 <rsancho@iesrioarba.es>
2023-12-14 13:22:27 +0100

    .

---
+++ README.md
@@ -0,0 +1,22 @@
+## btree.w.lists.py
+
+Una libreria para representar arboles binarios como listas.
+
+```bash
+*vacio* [none,none,none]
+
+22 : [22,none,none]
+
+  22
+11    [22,[11,none,none],none]
+
+  22
+11  33    [22,[11,none,none],[33,none,none]]
+
+  22
+    33    [22,none,[33,none,none]]
+
+*BAD* : [22,[33,none,none]]
+```
+
+
---
+++ btree.py
@@ -0,0 +1,17 @@
+
+
+def isTree(algo) -> bool:
+    # if not list -> False
+    # if len != 3 -> False
+    # recursion ...
+    # -> True
+
+if __name__ == "__main__":
+
+    bt = [None, None, None]
+    bt = [22, None, None]
+    bt = [22, [11, None, None], None]
+    bt = [22, [11, None, None], [33, None, None]]
+    # *BAD* : [22, [33, None, None]]
+
+
